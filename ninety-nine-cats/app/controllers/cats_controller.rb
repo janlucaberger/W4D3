1bbea238526eb31a176077799887816b934ac98c
mjohnson324 <mjohnson324@gmail.com>
2017-07-26 14:25:38 -0400

    Create forms to add and update cat

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -6,7 +6,41 @@ class CatsController < ApplicationController
 
   def show
     @cat = Cat.find(params[:id])
-
     render :show
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    cat = Cat.new(cat_params)
+    if cat.save
+      redirect_to cats_url
+    else
+      render json: cat.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+
+    if cat.update_attributes(cat_params)
+      redirect_to cats_url
+    else
+      render json: cat.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
 end
