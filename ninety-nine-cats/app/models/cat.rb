e92f040714f1d18b9d727a46dbce01a9718ee90f
mjohnson324 <mjohnson324@gmail.com>
2017-07-26 12:06:37 -0400

    Create cats table, model, route and controller, and seed table with cats

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,2 +1,15 @@
 class Cat < ActiveRecord::Base
+  CAT_COLORS = ["white", "black", "brown"]
+  validates :birth_date, :color, :sex, :name, :description, presence: true
+  validates :color, inclusion: { in: CAT_COLORS }
+  validates :sex, inclusion: { in: %w(M F) }
+
+  def self.colors
+    CAT_COLORS
+  end
+
+  def age
+    current_date = Time.now
+    current_date.year - self.birth_date.year
+  end
 end
--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sex = ["M", "F"]
+
+Cat.destroy_all
+cats = []
+10.times do
+  cats << Cat.create!(birth_date:  Faker::Date.birthday(1, 10), color: Cat.colors.sample, name: Faker::Cat.name, sex: sex.sample , description: "The best cat")
+end
